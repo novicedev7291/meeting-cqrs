88daf21bdd34dad15203b132ddfa801a95824a50
ni3 <kuldeep.yadav@noosyntech.in>
2021-02-14 23:14:33 +0530

    Initial structure

---
+++ meeting-svc/src/main/java/com/example/meeting/core/MeetingApplication.java
@@ -0,0 +1,11 @@
+package com.example.meeting.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MeetingApplication.class);
+    }
+}
